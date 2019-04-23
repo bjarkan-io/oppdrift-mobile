e03ca02d844490f5a489793fe5439430760f4627
Arne Molland <arneolaim@gmail.com>
2019-04-23 20:19:27 +0200

    :construction_worker: Travis & Codecov setup

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+runReport() {
+    if [ -f "coverage/lcov.info" ] && ! [ "$TRAVIS" ]; then
+        genhtml coverage/lcov.info -o coverage --no-function-coverage -s -p `pwd`
+        open coverage/index.html
+    fi
+}
+
+flutter packages get
+flutter packages pub run build_runner build --delete-conflicting-outputs
+flutter test --coverage
+runReport
