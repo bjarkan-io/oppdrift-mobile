9c043704fd973ee29025131a3c4fbc2405465505
Arne Molland <arneolaim@gmail.com>
2019-05-02 10:48:20 +0200

    :bento: Assets

---
+++ lib/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Oppdrift {
+  static get theme {
+    return ThemeData(
+      primaryColor: Color(0xFF092540),
+      accentColor: Color(0xFFFF0266),
+      fontFamily: 'Futura'
+    );
+  }
+}
